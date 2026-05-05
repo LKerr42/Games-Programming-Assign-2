e828b3ce2520334ba21d6ba7e7179ef6230e46fa
Liam Kerr <LiamKerr42@outlook.com>
2026-05-05 12:51:59 +1200

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <engine.h>
+#include <iostream>
+#include <vector>
+
+// Initialise (called once at start)
+void init() {
+    // Example
+    setWindowTitle("[game name] - Menu");
+
+}
+
+// Update Game
+void update(float dt) {
+
+}
+
+// Render Game
+void render(float lag) {
+
+}
+
+// Close the Game
+void close() {
+
+}
